6c92aaa4632089c7d0a941d23f47214166221a15
OpenShift guest <magnus.nordby@gmail.com>
2013-09-26 17:48:50 -0400

    New Grunt build. Config changes. config changes.

--- server/server.js
+++ server/server.js
@@ -67,10 +67,10 @@ require('./lib/routes/appFile').addRoutes(app, config);
 app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 
 // Start up the server on the port specified in the config
-server.listen(config.server.listenPort, 'localhost', 511, function() {
+server.listen(config.server.listenPort, process.env.IP, 511, function() {
   // // Once the server is listening we automatically open up a browser
   var open = require('open');
-  open('http://localhost:' + config.server.listenPort + '/');
+  open('http://' + process.env.IP + ':' + config.server.listenPort + '/');
 });
 console.log('Angular App Server - listening on port: ' + config.server.listenPort);
 secureServer.listen(config.server.securePort);
